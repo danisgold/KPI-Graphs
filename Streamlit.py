e096ac8c857742079c55cafee565a485df47558a
Danny Goldberg <94162501+danisgold@users.noreply.github.com>
2022-06-30 10:07:29 -0700

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -27,6 +27,13 @@ for uploaded_file in uploaded_files:
 import numpy as np
 import matplotlib.pyplot as plt
 
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
+
+
 
 # data to plot
 n_groups = 4
